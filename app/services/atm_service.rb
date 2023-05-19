441fae7cdfadf69c04ce7ac84b2db26226638853
Young Koh <youngheon6@gmail.com>
2023-05-18 18:07:52 -0600

    test: add more tests

--- app/services/atm_service.rb
+++ app/services/atm_service.rb
@@ -1,6 +1,12 @@
 require 'faraday'
 
 class AtmService
+  
+  def get_nearest_atms(object)
+    get_url("/search/2/categorySearch/atm.json?key=#{ENV['TOMTOM_API_KEY']}&lat=#{object.lat}&lon=#{object.lon}")
+  end
+
+  private
   def conn
     Faraday.new(url: "https://api.tomtom.com")
   end
@@ -9,10 +15,4 @@ class AtmService
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
   end
-
-
-  def get_nearest_atms(object)
-    get_url("/search/2/categorySearch/atm.json?key=#{ENV['TOMTOM_API_KEY']}&lat=#{object.lat}&lon=#{object.lon}")
-  end
-  
 end
